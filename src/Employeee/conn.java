ef57e543bea2b4e9fa0648e66cdc5ffcb6b6c3b5
Vidhinpatel08 <vidhin1208@gmail.com>
2022-08-26 23:54:44 +0530

    Updated With Final Modification

--- src/Employeee/conn.java
+++ src/Employeee/conn.java
@@ -2,19 +2,19 @@ package Employeee;
 
 import java.sql.*;
 
-public class conn{
-    
+public class conn {
+
     public Connection c;
     public Statement s;
- 
-    public conn(){
-        try{
+
+    public conn() {
+        try {
             Class.forName("com.mysql.jdbc.Driver");
-            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/project3","root","");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/project3", "root", "");
+            // if you change your xampp port then change here & username and password match here . 
             s = c.createStatement();
-        }catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
- 
